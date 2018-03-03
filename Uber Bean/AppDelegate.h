963d1fbbeea433a5513b43983ba69f325c1b1a22
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-03-02 16:36:31 -0800

    Initial Commit

---
+++ Uber Bean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uber Bean
+//
+//  Created by Chris Dean on 2018-03-02.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Uber Bean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Uber Bean
+//
+//  Created by Chris Dean on 2018-03-02.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
